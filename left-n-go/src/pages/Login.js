0569a25361a3b6f67b6dd6bad83b29900c645719
t11-20 <lgnd6090@gmail.com>
2022-05-29 06:18:12 +0800

    Completed Basic Login features
    Completed basic login features, will add in authentication along with database

--- left-n-go/src/pages/Login.js
+++ left-n-go/src/pages/Login.js
@@ -1,15 +1,155 @@
+import { IconButton, InputLabel, Input, InputAdornment, Button } from '@mui/material';
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from 'react';
+
 const Login = () => {
-    return ( 
-        <div className="login">
-            <h2>Login</h2>
-            <div style = {{display: "flex",flexFlow: "row nowrap"}}>
-                <div className="HeaderBox" style={{flex: 1}}><h2>Placeholder 1 (For Products)</h2></div>
-                <div className="HeaderBox" style={{flex: 1, textAlign: "right"}}><h2>Placeholder 2 (For Cart/Checkout)</h2></div>
+    const [passValues, setPassValues] = useState({
+        password: "",
+        showPassword: false,
+    });
+
+    const [emailValues, setEmailValues] = useState({
+        email: "",
+    });
+
+    const [loginDetails, setLoginDetails] = useState(
+        []
+    );
+
+    const handleClickShowPassword = () => {
+        setPassValues({ ...passValues, showPassword: !passValues.showPassword });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handlePasswordChange = (prop) => (event) => {
+        setPassValues({ ...passValues, [prop]: event.target.value });
+        // console.log("current password ," + passValues.password);
+        // console.log("setted password ," + setPassValues.password);
+    };
+
+    const handleEmailChange = (prop) => (event) => {
+        setEmailValues({ ...emailValues, [prop]: event.target.value });
+        // console.log("current email ," + emailValues.email);
+        // console.log("setted email ," + setEmailValues.email);
+    };
+
+    // const [name, setName] = useState("");
+    // onClick = (() => {
+    //     const newEmail = prompt("Enter your email");
+    // });
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        
+        const loginData = {
+            email: emailValues,
+            password: passValues
+        }
+        setLoginDetails(loginData);
+
+        console.log(loginData);
+    };
+
+    // const onSubmit = (data) => {
+    //     console.log(data);
+    //   };
+
+return (
+	<div
+	className="login">
+    
+    <h2>Welcome Back</h2>
+    <p>Login with your email & password</p>
+
+    <form onSubmit={handleSubmit}>
+        {/* For Email */}
+        <InputLabel htmlFor="email">
+            Enter your Email
+        </InputLabel>
+        <Input sx={{width:'25ch'}}
+            // type={emailValues.showPassword ? "text" : "password"}
+            //Upon onChange event, the email is setted.
+            onChange={handleEmailChange("email")}
+            value={emailValues.email}
+        />
+
+        {/* For Password */}
+        <InputLabel htmlFor="standard-adornment-password">
+            Enter your Password
+        </InputLabel>
+        <Input sx={{width:'25ch'}} style={{
+            marginBottom: "30px",
+        }}
+            type={passValues.showPassword ? "text" : "password"}
+            onChange={handlePasswordChange("password")}
+            value={passValues.password}
+            endAdornment={
+            <InputAdornment position="end">
+                <IconButton
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                >
+                {passValues.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+            </InputAdornment>
+            }
+        />
+            <div><Button sx={{width:'29ch'}} 
+            style={
+                {
+                    // background: "#f1356d",
+                    background: "#5297FF",
+                    color: "#fff",
+                    border: "0",
+                    padding: "8px",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                }
+            } 
+            type="submit"
+            >Login</Button></div>
+
+            <div className="otherLogin">
+                <p style={{
+                color: "#000000",
+                marginTop: "30px",
+                textAlign: "center",
+                fontSize: "15px",
+                // margin-bottom: 30px;
+                // width: auto;
+                }}>Login with other methods</p>
             </div>
-            <p>To continue with firebase auth, coupled with registration</p>
-        </div>
-     );
-}
- 
+        </form>
+	</div>
+    
+);
+};
+
 export default Login;
+
+// const Login = () => {
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+
+//     return ( 
+//         <div className="login" align="center">
+
+//             <h2>Login</h2>
+            
+//             <p>To continue with firebase auth, coupled with registration</p>
+//         </div>
+//      );
+// }
+ 
+// export default Login;
+
+// <div
+// 	style={{
+// 		marginLeft: "30%",
+//         align: "center",
+// 	}}
+// 	></div>
 
