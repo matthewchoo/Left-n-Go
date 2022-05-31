db3be33609d1cd7eef27a6c35d6bb843784b187d
t11-20 <lgnd6090@gmail.com>
2022-06-01 04:43:20 +0800

    Updated firebase authentication
    Updated firebase authentication from personal email to shared email

--- left-n-go/src/config/firebaseConfig.js
+++ left-n-go/src/config/firebaseConfig.js
@@ -4,13 +4,22 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyCRXOXAINAqQA8uejRiwKQ7S0kWjAM-j8U",
+//   authDomain: "left-n-go-1.firebaseapp.com",
+//   projectId: "left-n-go-1",
+//   storageBucket: "left-n-go-1.appspot.com",
+//   messagingSenderId: "1057582981803",
+//   appId: "1:1057582981803:web:9cfa1ea847aeb12adf0dee"
+// };
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCRXOXAINAqQA8uejRiwKQ7S0kWjAM-j8U",
-  authDomain: "left-n-go-1.firebaseapp.com",
-  projectId: "left-n-go-1",
-  storageBucket: "left-n-go-1.appspot.com",
-  messagingSenderId: "1057582981803",
-  appId: "1:1057582981803:web:9cfa1ea847aeb12adf0dee"
+  apiKey: "AIzaSyCgNmHXJorJ3s7welJS5Cx25dTokn0kKco",
+  authDomain: "left-n-go.firebaseapp.com",
+  projectId: "left-n-go",
+  storageBucket: "left-n-go.appspot.com",
+  messagingSenderId: "932667893902",
+  appId: "1:932667893902:web:a1ddebc74ec1dd2cc3c1df"
 };
 
 // Initialize Firebase
