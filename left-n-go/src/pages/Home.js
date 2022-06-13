048effff8ddcaad004f6a376899225ac77bbf7b1
Matt Choo <mattchoo@matts-air.lan>
2022-06-13 22:12:59 +0800

    Added on more CSS
    Adding a main component to help show the products and CSS to help change it's looks

--- left-n-go/src/pages/Home.js
+++ left-n-go/src/pages/Home.js
@@ -3,7 +3,15 @@ const Home = () => {
         <div className="home">
             <h2>Homepage</h2>
             <div style = {{display: "flex",flexFlow: "row nowrap"}}>
-                <div className="HeaderBox" style={{flex: 1}}><h2>Placeholder 1 (For Products)</h2></div>
+                <div className="HeaderBox" style={{flex: 1}}>
+                    <h2 class = 'pTitle'>Products</h2>
+                    <div id='product-catalog'><h1>hi</h1></div>
+                </div>
+                
+                
+                
+                
+                
                 <div className="HeaderBox" style={{flex: 1, textAlign: "right"}}><h2>Placeholder 2 (For Cart/Checkout)</h2></div>
             </div>
             {/* <p>To be Continued.</p> */}
