7f67a3035ec2657299f0eab6732ff9ebad6a4dcd
t11-20 <lgnd6090@gmail.com>
2022-07-10 09:21:49 +0800

    Added useCollection.js
    useCollection.js is for real-time database

---
+++ left-n-go/src/hooks/useCollection.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { db } from "../firebase/config"
+
+//firebase imports
+import { collection, onSnapshot } from "firebase/firestore"
+
+
+export const useCollection = (collectionName) => {
+    const [ documents, setDocuments ] = useState(null)
+    
+    useEffect(() => {
+        let ref = collection(db, collectionName)
+
+        const unsub = onSnapshot(ref, (snapshot) => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+                results.push({...doc.data(), id: doc.id })
+            })
+
+            setDocuments(results)
+        })
+
+        return () => unsub()
+
+    }, [collectionName])
+
+    return { documents }
+}
